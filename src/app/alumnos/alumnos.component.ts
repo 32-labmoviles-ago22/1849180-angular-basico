16ad1c49a85248134f15f13be011721f3d1b5f1e
Alissa <alissa.faen@gmail.com>
2022-10-11 20:31:42 -0500

    Sliding Options y Action Sheet

--- src/app/alumnos/alumnos.component.ts
+++ src/app/alumnos/alumnos.component.ts
@@ -1,87 +1,155 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
+
+interface alumnoasistencia {
+  nombre: string;
+  presente: boolean;
+}
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
-  styleUrls: ['./alumnos.component.scss']
+  styleUrls: ['./alumnos.component.css']
 })
 export class AlumnosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertaController : AlertController, private actionSheetController : ActionSheetController) { }
 
   ngOnInit(): void {
-
-    console.log(this.alumnos);
+    //console.log(this.alumnos);
   }
-    alumnos: any = [
+
+  async mensajeAlerta(tipoMensaje: string){
+    const alerta = this.alertaController.create(
+      {
+        header: 'ATENCIÓN',
+        subHeader: 'ESTO ES ' + tipoMensaje,
+        message: 'SALIR',
+        buttons: ['OK'],
+      }
+    );
+
+    (await alerta).present();
+
+  }
+
+  async abrirActionSheet(){
+    const actionSheet = this.actionSheetController.create({
+      header: 'Yo soy la ActionSheet',
+      subHeader: '',
+      buttons: [
+        {
+          text: 'Borrar',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+
+        {
+          text: 'Compartir',
+          data:{
+            action: 'compartir',
+          },
+        },
+
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data:{
+            action: 'cancelar',
+          },
+        },
+        
+      ]
+    });
+
+    (await actionSheet).present();
+
+  }
+
+
+  alumnos: any = [
       {
         nombre: "Edgar Efren",
-        presente: false
+        presente: false,
       },
       
       {
-        nombre: "Isaac Briones",
-        presente: false
+        nombre: "Isaac Zuñiga",
+        presente: true,
       },
       
       {
         nombre: "Orlando Castillo",
-        presente: true
+        presente: true,
       },
+  
       {
         nombre: "Jose David",
-        presente: true
+        presente: true,
       },
+  
       {
         nombre: "Jesus Gerardo",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Raul Eduardo",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Cesar Alexandro",
-        presente: true
+        presente: false,
       },
+      
       {
         nombre: "Andres Hernandez",
-        presente: true
+        presente: false,
       },
+      
       {
         nombre: "Antonio Medina",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Mauricio Nicolas",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Fidel Ortiz",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Manuel Rico Luna",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Jonathan David",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Felipe Daniel",
-        presente: true
+        presente: false,
       },
+      
       {
         nombre: "Luis Antonio Rocha",
-        presente: true
+        presente: true,
       },
+      
       {
         nombre: "Luis Fernando Caamal",
-        presente: true
+        presente: false,
       },
-      
-      
       
     ]
 }
